a4cdbd15a2fe377dabb9c5acb030fa066fbc2aa3
roberto_farias <75376776+roberto06rhz@users.noreply.github.com>
2023-06-01 10:14:00 -0300

    ....

---
+++ backend-esqueleto/src/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const dbSequelize = new Sequelize('aluno', 'aluno.ifal', 'aluno.ifal', {
+    dialect: 'mysql', host: 'localhost'
+});
+
+//const dbSequelize = new Sequelize(process.env.DATABASE_URL);
+
+module.exports = dbSequelize;
